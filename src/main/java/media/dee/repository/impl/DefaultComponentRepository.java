1ef909ae4309d7e7dd37fe54907c13f42d873f71
Farhan <syed@alaan.tv>
2018-01-28 18:33:55 +0400

    repository impl updates

--- src/main/java/media/dee/repository/impl/DefaultComponentRepository.java
+++ src/main/java/media/dee/repository/impl/DefaultComponentRepository.java
@@ -1,6 +1,9 @@
 
 package media.dee.repository.impl;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import media.dee.core.repository.api.ComponentRepository;
 import media.dee.repository.internal.CypherExecutor;
 import org.neo4j.driver.v1.Record;
@@ -10,6 +13,8 @@ import org.osgi.service.component.annotations.Reference;
 import java.util.Collections;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Component(immediate = true, service = ComponentRepository.class)
 public class DefaultComponentRepository  implements ComponentRepository {
@@ -18,16 +23,38 @@ public class DefaultComponentRepository  implements ComponentRepository {
 
     private CypherExecutor queryExecutor;
 
+    private LoadingCache<String, String> queryCache;
+    private void buildCache() {
+        queryCache = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.MINUTES)
+                                 .maximumSize(1000)
+                                 .build(new CacheLoader<String, String>() {
+                                @Override
+                                public String load(String componentID) throws Exception {
+                                    String qry = "MATCH(c:Component{id:{componentID}}) RETURN C.query as query";
+                                     Map result = queryExecutor.fetchOne(qry, Collections.singletonMap("componentID",componentID)).asMap();
+                                     return result.get("query").toString();
+                                }
+                            });
+    }
+
+
     @Reference
     public void setCypherExecutor(CypherExecutor executor){
         this.queryExecutor = executor;
+        buildCache();
     }
 
     @Override
-    public Map<String,Object> findOne(String ComponentID, Map context) {
+    public Map<String,Object> findOne(String componentID, Map context) {
+        try {
+            String query = queryCache.get(componentID);
+            Record record = this.queryExecutor.fetchOne(query, context);
+            return record.asMap();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
-        Record record = this.queryExecutor.fetchOne(queries.getString("Component.By.Id"), Collections.emptyMap());
-        return record.asMap();
+        return Collections.emptyMap();
     }
 
 }
